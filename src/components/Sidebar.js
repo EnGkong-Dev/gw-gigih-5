d43ed75de879a7640fcbd26fc07fba5d68a710b5
EnGkong-Dev <randyrf@student.gunadarma.ac.id>
2022-03-25 12:41:08 +0700

    dawdawdaw

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -41,7 +41,6 @@ function Sidebar() {
             <p className='hover:text-white'>🎶 2020 Playlist Top</p>
             <p className='hover:text-white'>😶‍🌫️ 2021 Playlist Top</p>
             <p className='hover:text-white'>🤍 2022 Playlist Top</p>
-            <p className='hover:text-white'>🤍 2023 Playlist Top</p>
         </div>
       </div>
   )
